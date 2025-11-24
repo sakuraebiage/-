318e8b80ce37d310eb0954e27570c129449e0dbf
sakuraebiage <hanahana1437.29@gmail.com>
2025-11-24 22:12:47 +0900

    Update header-loader.js

--- common/header-loader.js
+++ common/header-loader.js
@@ -2,20 +2,8 @@
   const headerContainer = document.getElementById("header-container");
   if (!headerContainer) return;
 
-  const path = window.location.pathname;
-
-  // 最初は同じ階層扱い
-  let headerPath = 'common/header.html';
-
-  // 下層ディレクトリ（1階層深い）判定
-  const subdirs = [
-    '/defense/', '/setting/', '/training/',
-    '/craft/', '/items/', '/chat/', '/mypage/', '/result/', '/log/'
-  ];
-
-  if (subdirs.some(dir => path.includes(dir))) {
-    headerPath = '../common/header.html';
-  }
+  // GitHub Pages のルートが "/-/" なので絶対パスで指定
+  const headerPath = "/-//common/header.html";
 
   fetch(headerPath)
     .then(res => {
@@ -27,3 +15,4 @@
     })
     .catch(err => console.error(err));
 })();
+
