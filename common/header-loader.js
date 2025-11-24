1beb6df2ebe58d7eb0d2a75059156b5c58eb5e3e
sakuraebiage <hanahana1437.29@gmail.com>
2025-11-24 22:09:34 +0900

    Update header-loader.js

--- common/header-loader.js
+++ common/header-loader.js
@@ -1,26 +1,29 @@
-(function(){
+(function () {
   const headerContainer = document.getElementById("header-container");
   if (!headerContainer) return;
 
-  let headerPath = 'common/header.html'; // デフォルト
-
-  // ページ階層による調整
   const path = window.location.pathname;
 
-  if (path.includes('/defense/')) headerPath = '../common/header.html';
-  else if (path.includes('/setting/')) headerPath = '../common/header.html';
-  else if (path.includes('/traning/')) headerPath = '../common/header.html';
-  else if (path.includes('/craft/')) headerPath = '../common/header.html';
-  else if (path.includes('/items/')) headerPath = '../common/header.html';
-  else if (path.includes('/chat/')) headerPath = '../common/header.html';
-  else if (path.includes('/mypage/')) headerPath = '../common/header.html';
-  else if (path.includes('/result/')) headerPath = '../common/header.html';
+  // 最初は同じ階層扱い
+  let headerPath = 'common/header.html';
+
+  // 下層ディレクトリ（1階層深い）判定
+  const subdirs = [
+    '/defense/', '/setting/', '/training/',
+    '/craft/', '/items/', '/chat/', '/mypage/', '/result/', '/log/'
+  ];
+
+  if (subdirs.some(dir => path.includes(dir))) {
+    headerPath = '../common/header.html';
+  }
 
   fetch(headerPath)
     .then(res => {
       if (!res.ok) throw new Error("ヘッダー読み込み失敗");
       return res.text();
     })
-    .then(html => { headerContainer.innerHTML = html; })
+    .then(html => {
+      headerContainer.innerHTML = html;
+    })
     .catch(err => console.error(err));
 })();
